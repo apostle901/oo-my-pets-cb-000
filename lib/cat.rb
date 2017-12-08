3cda8ded9ada261fab996dddda2b6434f1c9a93c
Jawann <jawann@gmail.com>
2017-12-08 03:22:10 +0000

    automatically backed up by learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,7 +1,13 @@
 class Cat
   attr_reader :name
+  attr_accessor :mood
   # code goes here
   def initialize(name)
     @name = name
+    @mood = nervous
+  end
+
+  def mood
+    self.mood
   end
 end
